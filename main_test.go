be4e3dbd460930baa10800b363c13f04afadf3f2
rizal <rizal@lomotif.com>
2020-07-04 16:10:11 +0800

    initial commit

---
+++ main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var router = setupRouter()
+
+func TestPing(t *testing.T) {
+	w := httptest.NewRecorder()
+	resp := struct {
+		Message string
+	}{}
+
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, req)
+
+	if err := json.Unmarshal(w.Body.Bytes(), &resp); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, resp.Message, "pong")
+}
